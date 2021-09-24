af23e03baa1b1dc1fe1849585b063def84ec9d9f
Thomas Landstra <thomaslandstra@gmail.com>
2021-09-24 18:36:20 +1000

    bullets work and enemies can be killed

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ GRAVITY = 0.75
 # Player Variables
 moving_left = False
 moving_right = False
+shoot = False
 
 # Images
 bullet_img = pygame.image.load("img/icons/bullet.png").convert_alpha()
@@ -47,7 +48,7 @@ def draw_bg():
 
 # Classes
 class Soilder(pygame.sprite.Sprite):
-    def __init__(self, char_type: str, x: int, y: int, scale, speed):
+    def __init__(self, char_type: str, x: int, y: int, scale, speed, ammo):
         # Inherit sprite class
         pygame.sprite.Sprite.__init__(self)
 
@@ -57,6 +58,12 @@ class Soilder(pygame.sprite.Sprite):
         self.in_air = True
         self.char_type = char_type
         self.speed = int(speed * 5)
+        self.ammo = ammo
+        self.start_ammo = ammo
+        self.shoot_cooldown = 0
+        self.fire_rate = 20  # Lower is better
+        self.health = 100
+        self.max_health = self.health
         self.direction = 1
         self.vel_y = 0
         self.flip = False
@@ -66,18 +73,23 @@ class Soilder(pygame.sprite.Sprite):
         self.update_time = pygame.time.get_ticks()
 
         # Add animations
-        animation_types = ["Idle", "Run", "Jump"]
+        animation_types = ["Idle", "Run", "Jump", "Death"]
         for animation in animation_types:
             temp_list = []
             number_of_frames = len(os.listdir(f"img/{self.char_type}/{animation}"))
+
             for i in range(number_of_frames):
+                # Load Image
                 img = pygame.image.load(
                     f"img/{self.char_type}/{animation}/{i}.png"
                 ).convert_alpha()
+
+                # Scale Image
                 img = pygame.transform.scale(
                     img, (int(img.get_width() * scale), int(img.get_height() * scale))
                 )
-                temp_list.append(img)
+
+                temp_list.append(img)  # Add Image to list
 
             self.animation_list.append(temp_list)
 
@@ -85,6 +97,12 @@ class Soilder(pygame.sprite.Sprite):
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+    def update(self):
+        self.update_animation()
+        self.check_alive()
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
 
     def move(self, moving_left, moving_right):
         # reset variables
@@ -126,6 +144,20 @@ class Soilder(pygame.sprite.Sprite):
         self.rect.x += dx
         self.rect.y += dy
 
+    def fire(self):
+        if self.shoot_cooldown == 0 and self.ammo > 0:
+            # Draw bullet
+            self.shoot_cooldown = self.fire_rate
+            bullet = Bullet(
+                self.rect.centerx + (self.rect.size[0] * 0.6 * self.direction),
+                self.rect.centery,
+                self.direction,
+            )
+            bullet_group.add(bullet)
+
+            # Reduce ammo
+            self.ammo -= 1
+
     def update_animation(self):
         # Update animation
         ANIMATION_COOLDOWN = 100
@@ -138,13 +170,23 @@ class Soilder(pygame.sprite.Sprite):
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
             if self.frame_index >= len(self.animation_list[self.action]):
-                self.frame_index = 0
+                if self.action == 3:
+                    self.frame_index = len(self.animation_list[self.action]) - 1
+                else:
+                    self.frame_index = 0
 
     def update_action(self, new_action):
         if new_action != self.action:
             self.action = new_action
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
+
+    def check_alive(self):
+        if self.health <= 0:
+            self.health = 0
+            self.speed = 0
+            self.alive = False
+            self.update_action(3)
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
@@ -159,29 +201,48 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.center = (x, y)
         self.direction = direction
 
+    def update(self):
+        # Move bullet
+        self.rect.x += self.speed * self.direction
+
+        # Is bullet off screen
+        if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
+            self.kill()
+
+        # Check character collision
+        if pygame.sprite.spritecollide(player, bullet_group, False) and player.alive:
+            player.health -= 5
+            self.kill()
+        if pygame.sprite.spritecollide(enemy, bullet_group, False) and enemy.alive:
+            enemy.health -= 25
+            self.kill()
+
 
 # Create sprite groups
 bullet_group = pygame.sprite.Group()
 
 # Create Assets
-player = Soilder("player", 200, 200, 3, 1)
-enemy = Soilder("enemy", 400, 200, 3, 1)
+player = Soilder("player", 200, 200, 3, 1, 20)
+enemy = Soilder("enemy", 400, 300, 3, 1, 20)
 
 run = True
 while run:
     clock.tick(FPS)
     draw_bg()
 
+    enemy.update()
     enemy.draw()
-    player.update_animation()
+    player.update()
     player.draw()
 
     # Update and draw groups
     bullet_group.update()
-    bullet_group.draw()
+    bullet_group.draw(screen)
 
     # Update player action
     if player.alive:
+        if shoot:
+            player.fire()
         if player.in_air:
             player.update_action(2)  # Run
         elif moving_left or moving_right:
@@ -204,8 +265,8 @@ while run:
                 moving_right = True
             if event.key == pygame.K_w and player.alive:
                 player.jump = True
-            if event.key == pygame.K_SPACE and player.alive:
-                player.jump = True
+            if event.key == pygame.K_SPACE:
+                shoot = True
             if event.key == pygame.K_ESCAPE:
                 run = False
 
@@ -215,6 +276,8 @@ while run:
                 moving_left = False
             if event.key == pygame.K_d:
                 moving_right = False
+            if event.key == pygame.K_SPACE:
+                shoot = False
 
     # Draw screen
     pygame.display.update()
